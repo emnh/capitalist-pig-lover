126939930e91f88b5fe92c1773518ab4ad1ff5be
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2019-01-25 02:47:51 +0100

    charts. evolve. bug fix.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 $ = require('jquery');
+d3 = require('d3');
+c3 = require('c3');
 
 function orClick(evt) {
   console.log("Hello");
@@ -41,20 +43,60 @@ function getMsgs() {
           date.toLocaleString() + " " + user + " > " +
           "<div>" +
           (m[context] !== undefined ? "<span>" + m[context] + "</span> " : "") +
-          "<input type='button' id='" + id +
+          "<input type='button' data-tag='" + m[choiceA] + "' id='" + id +
           "' style='display: inline-block; padding: 10px; border: 1px solid black;' value='" + m[choiceA] +
           " (" + choice1 + ")" +
+          //"'>" + "<span class='sub' id='" + m[choiceA] + "'" + "</span>" + "</input>" +
           "'></input>" +
+          "<div style='display: inline-block; position: absolute; top: 15px;' id='chart1" + i + "'></div>" +
           " or " +
-          "<input type='button' id='" + id +
+          "<input type='button' data-tag='" + m[choiceB] + "' id='" + id +
           "' style='display: inline-block; padding: 10px; border: 1px solid black;' value='" + m[choiceB] +
           " (" + choice2 + ")" +
+          //"'>" + "<span class='sub' id='" + m[choiceB] + "'" + "</span>" + "</input>" +
           "'></input>" +
+          "<div style='display: inline-block; position: absolute; top: 15px;' id='chart2" + i + "'></div>" +
           "?" +
           "</div>" +
           "</div>";
         $("#messages")
           .append("<li " + listStyle + ">" + block + "</li>")
+        if (t > 0) {
+          const genChart = function(choiceNum) {
+            const colors = {};
+            colors[m[choiceA]] = choiceNum == 1 ? '#0000FF' : '#FFFFFF';
+            colors[m[choiceB]] = choiceNum == 1 ? '#FFFFFF' : '#0000FF';
+            c3.generate({
+              bindto: "#chart" + choiceNum + "" + i,
+              size: {
+                width: 40,
+                height: 40
+              },
+              data: {
+                  columns: [
+                      [m[choiceA], c1c],
+                      [m[choiceB], c2c],
+                  ],
+                  colors: colors,
+                  type: 'pie',
+              },
+              legend: {
+                show: false
+              },
+              pie: {
+                label: {
+                  format: function(value, ratio, id) {
+                    return '';
+                  }
+                }
+              }
+            });
+          };
+          genChart(1);
+          genChart(2);
+          $("#chart1" + i + " .c3-chart-arc path").css("stroke", '#000000');
+          $("#chart2" + i + " .c3-chart-arc path").css("stroke", '#000000');
+        }
       } else {
         $("#messages").append("<li " + listStyle + ">" + dp[i].msg + "</li>")
       }
@@ -64,7 +106,7 @@ function getMsgs() {
       .click(function(evt) {
         $.post("postclick", {
           msgid: evt.target.id,
-          tag: $(evt.target).val()
+          tag: evt.target.dataset.tag
         }, function(data) {
           getMsgs();
           //console.log("response", data);
